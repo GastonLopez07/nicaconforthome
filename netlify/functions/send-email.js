6a9605129ce5c2d2a67bb43ae2a186fdb4451148
GastonLopez <lopezgaston45@gmail.com>
2026-03-18 12:23:11 -0300

    Modificaciones

--- netlify/functions/send-email.js
+++ netlify/functions/send-email.js
@@ -1,12 +1,28 @@
-import { Resend } from "resend";
+const { Resend } = require("resend");
 
-const resend = new Resend("re_FSz6PkRQ_FjDBdcxgKSLqgFs1oDayaz4c");
+const resend = new Resend(process.env.RESEND_API_KEY);
 
-await resend.emails.send({
-  from: "Nica Confort Home <onboarding@resend.dev>",
-  to: [email],
-  subject: "Tu hogar más confortable empieza acá 🏡",
-  html: `
+exports.handler = async function (event) {
+  try {
+    const { email } = JSON.parse(event.body);
+
+    // 📩 Mail para vos
+    await resend.emails.send({
+      from: "Nica Confort Home <onboarding@resend.dev>",
+      to: ["TUEMAIL@gmail.com"],
+      subject: "Nuevo cliente potencial 🚀",
+      html: `
+        <h2>Nuevo lead</h2>
+        <p><strong>Email:</strong> ${email}</p>
+      `,
+    });
+
+    // 📩 Mail para el usuario
+    await resend.emails.send({
+      from: "Nica Confort Home <onboarding@resend.dev>",
+      to: [email],
+      subject: "Tu hogar más confortable empieza acá 🏡",
+      html: `
     <div style="font-family: Arial, sans-serif; line-height:1.6; color:#333;">
       
       <h2 style="color:#111;">Bienvenido a Nica Confort Home 👋</h2>
@@ -14,7 +30,7 @@ await resend.emails.send({
       <p>Gracias por contactarte.</p>
 
       <p>
-        Gracias por sumarte! Tenes un 20% OFF!!.
+        Ya tenes el 20% OFF!!
       </p>
 
       <hr style="margin:20px 0;">
@@ -22,7 +38,7 @@ await resend.emails.send({
       <p><strong>¿Necesitás ayuda ahora?</strong></p>
 
       <p>
-        Podés escribirnos directamente a "nicaconforthome@gmail.com" y lo vemos sin vueltas.
+        Podés escribirnos directamente a nicaconforthome@gmail.com y lo vemos sin vueltas.
       </p>
 
       <br/>
@@ -33,4 +49,18 @@ await resend.emails.send({
 
     </div>
   `,
-});
+    });
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ ok: true }),
+    };
+  } catch (error) {
+    console.error("ERROR:", error);
+
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: error.message }),
+    };
+  }
+};
